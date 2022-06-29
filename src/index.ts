7fce24764deb5a4d6cb9656efa4e2a6396358ccb
Rukundo Kevin <kevinrukundo1@gmail.com>
2022-06-29 11:50:13 +0200

    Optimize code for production

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,23 @@
 import { ApolloServer } from "apollo-server";
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-import { context } from "./context";   
+import {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+  ApolloServerPluginLandingPageLocalDefault,
+} from "apollo-server-core";
+import { context } from "./context";
 
 import { schema } from "./schema";
+
 export const server = new ApolloServer({
-    schema,
-    context,
-    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  schema,
+  context,
+  introspection: true,
+  plugins: [
+    ApolloServerPluginLandingPageGraphQLPlayground(),
+    ApolloServerPluginLandingPageLocalDefault(),
+  ],
 });
 
-const port = 4000;
+const port = process.env.PORT || 3000;
 
 server.listen({port}).then(({ url }) => {
     console.log(`🚀  Server is running at ${url}`);
